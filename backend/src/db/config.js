b6fba64cc465bfb08f774dd195b24880cb08afc6
Ben DiFrancesco <ben@scopelift.co>
2020-04-05 07:11:19 -0400

    Setup communication to Postgres
    * Install Sequelize and associate depedencies and
      configure it
    * Add npm script for setting up initial db users and
      databases

---
+++ backend/src/db/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+
+module.exports = {
+
+  development: {
+    database: process.env.DEV_DB_NAME,
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    host: process.env.DEV_DB_HOST,
+    dialect: 'postgres'
+  },
+
+  test: {
+    database: process.env.TEST_DB_NAME,
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASS,
+    host: process.env.TEST_DB_HOST,
+    dialect: 'postgres'
+  },
+
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};
