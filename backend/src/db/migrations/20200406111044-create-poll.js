6a458cea57acea0b0c9e9b0745a2e6a845920d05
Ben DiFrancesco <ben@scopelift.co>
2020-04-09 16:07:50 -0400

    Disallow nullability for poll title and polloption contents

--- backend/src/db/migrations/20200406111044-create-poll.js
+++ backend/src/db/migrations/20200406111044-create-poll.js
@@ -9,6 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       title: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
--- backend/src/db/migrations/20200408103226-create-poll-options.js
+++ backend/src/db/migrations/20200408103226-create-poll-options.js
@@ -10,6 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       contents: {
+        allowNull: false,
         type: Sequelize.STRING(1000)
       },
       createdAt: {
