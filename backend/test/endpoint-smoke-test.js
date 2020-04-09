044424c80791811429163db617891936b2446b6b
Ben DiFrancesco <ben@scopelift.co>
2020-04-09 12:37:33 -0400

    Add endpoint smoke tests for required fields

--- backend/test/endpoint-smoke-test.js
+++ backend/test/endpoint-smoke-test.js
@@ -9,6 +9,39 @@ const { expect } = chai;
 
 describe('Smoke Testing Endpoints', () => {
 
+    it('should fail to create a poll missing a title', (done) => {
+        const poll = {
+            fancy_id: 'the-first-cool-poll',
+            polltaker_account: '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+            description: 'This could be a very, very long amount of text if we wanted it to be I guess',
+            start_date: (new Date()),
+            end_date: (new Date()),
+            valid_event_ids: [1, 3, 5,],
+            poll_options: [
+                {
+                    contents: 'The first cool option',
+                },
+                {
+                    contents: 'The second cool option',
+                },
+            ]
+        };
+
+        chai.request(app)
+            .post('/api/polls')
+            .set('Accept', 'application/json')
+            .send(poll)
+            .end( (_error, result) => {
+                expect(result.status).to.equal(400);
+                expect(result.body).to.include({
+                    error: 'Missing required poll data fields',
+                });
+
+                done();
+            });
+    });
+
+
     it('should create a poll', (done) => {
         const poll = {
             title: 'The first cool poll',
@@ -56,6 +89,27 @@ describe('Smoke Testing Endpoints', () => {
                 expect(result.body.length).to.equal(1);
                 expect(result.body[0].title).to.equal('The first cool poll');
                 expect(result.body[0].poll_options.length).to.equal(2);
+
+                done();
+            });
+    });
+
+    it('should fail to create a vote missing a poll option', (done) => {
+        const vote = {
+            voter_account: "0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF",
+            token_ids: [10, 2, 27],
+            date_cast: 1585773069000,
+        };
+
+        chai.request(app)
+            .post('/api/votes/the-first-cool-poll')
+            .set('Accept', 'application/json')
+            .send(vote)
+            .end( (_error, result) => {
+                expect(result.status).to.equal(400);
+                expect(result.body).to.include({
+                    error: 'Missing required vote data fields',
+                });
 
                 done();
             });
