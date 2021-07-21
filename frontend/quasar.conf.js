3524a35e997eb9f488b02c878aac98fa11eb6569
Sebas Favaron <sebastianfavaron@gmail.com>
2021-07-21 11:12:08 -0300

    add counter url to conf

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -86,6 +86,7 @@ module.exports = function (ctx) {
       env: {
         INFURA_ID: JSON.stringify(process.env.INFURA_ID),
         BUILD_ENV: JSON.stringify(process.env.NODE_ENV),
+        VUE_APP_GOAT_COUNTER:  '"https://poap-vote.goatcounter.com/count"',
       },
     },
 
