e4e0577d3070db00623e12067490d359f3562191
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2022-04-26 14:30:46 -0300

    Rollbacking changes in backend

--- backend/src/poap/index.js
+++ backend/src/poap/index.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 class POAP {
 
     static async fetchEvents() {
-        const response = await axios.get('https://api.poap.tech/events');
+        const response = await axios.get('https://api.poap.xyz/events');
         return response.data;
     }
 
     static async fetchTokens(account) {
-        const response = await axios.get(`https://api.poap.tech/actions/scan/${account}`);
+        const response = await axios.get(`https://api.poap.xyz/actions/scan/${account}`);
         return response.data;
     }
 }
