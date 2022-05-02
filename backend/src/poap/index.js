48294c697d2a48e05345d37797491a06f6f1a18d
Nicolás de León <42281395+nicolasdeleon@users.noreply.github.com>
2022-05-02 18:58:35 -0300

    ✨ Feature: Use events from s3 (#85)
    * Backend - Consume events from s3 bucket
    
    * Front - Consume events from s3 bucket
    
    Co-authored-by: Nicolas de Leon <nicolasmatiasdeleon@gmail.com>

--- backend/src/poap/index.js
+++ backend/src/poap/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 class POAP {
 
     static async fetchEvents() {
-        const response = await axios.get('https://api.poap.tech/events');
+        const response = await axios.get('https://api-event.poap.xyz');
+
         return response.data;
     }
 
